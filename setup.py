6191c4db0ea99bb1f29941e297f9eaa87e25ef66
Francis Lavoie <f.lavoie@onf.ca>
2014-09-26 18:17:59 -0400

    added setup, need work to install executable, work in progress

---
+++ setup.py
@@ -0,0 +1,25 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+try:
+    from setuptools import find_packages
+except ImportError:
+    from distutils.core import find_packages
+
+packages = find_packages()
+
+setup(
+    name = "file-stalker",
+    version = "0.0.2",
+    author = "Francis Lavoie",
+    author_email = "lav.francis@gmail.com",
+    description = "Skalk a directory and run a command when something change",
+    license ="MIT License",
+    url = "git@github.com:francisl/file-stalker.git",
+    packages = [
+        "src"
+    ],
+
+    install_requires = []
+)
